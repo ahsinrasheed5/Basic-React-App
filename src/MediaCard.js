aed7a56b1763b1a8f5c07c35000a8661dd9874df
ahsinrasheed5 <65343061+ahsinrasheed5@users.noreply.github.com>
2020-06-07 14:07:54 +0100

    done the task

--- src/MediaCard.js
+++ src/MediaCard.js
@@ -5,7 +5,7 @@ const MediaCard = () =>{
 return <div>
         <h2>M.Ahsin 's Task</h2>
         <p>This is your turn  Example from <strong> Daveceddia </strong> </p>
-        <img  src="./flag.jpg" width="200px" height="200px" />
+        <img  src="./flag.jpg" width="200px" height="200px"  alt = "pic" />
         </div>
 }
 
