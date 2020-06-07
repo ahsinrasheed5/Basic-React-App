ed357e94c0bdc0cdca4d4adbd7f387a46c5d5ae3
ahsinrasheed5 <65343061+ahsinrasheed5@users.noreply.github.com>
2020-06-07 23:18:27 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./index.css";
-import MediaCard from './MediaCard.js';
+// import MediaCard from './MediaCard.js';
 
 const  Hi = (props) =>{
   return <div>
