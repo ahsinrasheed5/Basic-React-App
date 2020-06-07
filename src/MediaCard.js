e9e9d78f24a98e433664b14c550e284c62276f33
ahsinrasheed5 <65343061+ahsinrasheed5@users.noreply.github.com>
2020-06-07 14:01:02 +0100

    updated

---
+++ src/MediaCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const MediaCard = () =>{
+return <div>
+        <h2>M.Ahsin 's Task</h2>
+        <p>This is your turn  Example from <strong> Daveceddia </strong> </p>
+        <img  src="./flag.jpg" width="200px" height="200px" />
+        </div>
+}
+
+export default MediaCard;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "./index.css";
+import "./MediaCard.js"
+import MediaCard from './MediaCard.js';
 
-function Hi() {
-  return <div>Hello World!</div>;
+const  Hi = (props) =>{
+  return <div>
+          <h1>Expample 1</h1>
+          Hello <strong>{props.name}</strong>
+          <h1 className="heading-1">Plain Function</h1>
+          <ul className= "list">
+            <li>Function</li>
+            <li>Arrow Function</li>
+          </ul>
+          <br/>
+          { 5 + 10}
+          <hr/>
+          <h1>Expample 2</h1>
+
+        </div>;
 }
 
-ReactDOM.render(<Hi/>, document.querySelector('#root'));
+ReactDOM.render(<Hi name="Sir M.Ahsin"/>, document.querySelector('#root'));
+ReactDOM.render(<MediaCard/>, document.querySelector('#sub-root'));
