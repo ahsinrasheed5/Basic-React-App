51f1172ef3d2a8682634bdc93847fdca9dd3a2a2
ahsinrasheed5 <65343061+ahsinrasheed5@users.noreply.github.com>
2020-06-07 23:49:26 +0100

    updated

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./App.css";
+
+const  Hi = (props) =>{
+  return <div>
+          <h1>Expample 1</h1>
+          Hello <strong>{props.name}</strong>
+          <h1 className="heading-1">Plain Function</h1>
+          <ul className= "list">
+            <li>Function</li>
+            <li>Arrow Function</li>
+          </ul>
+          <br/>
+          { 5 + 10}
+          <hr/>
+          <h1>Expample 2</h1>
+          <h2>M.Ahsin 's Task</h2>
+          <p>This is your turn  Example from <strong> Daveceddia </strong> </p>
+          <img  src="./flag.jpg" width="200px" height="200px"  alt = "pic" />
+        </div>;
+}
+
+export default Hi ;
